5a16bcca23b65f3ed49de193abd12d3d19dd0189
pigudogzyy <21301050051@m.fudan.edu.cn>
2024-05-02 10:36:40 +0800

    update

--- test.R
+++ test.R
@@ -23,6 +23,11 @@ adata = readRDS("./data/data_M_annotation.rds")
 
 save(adata,file="./data/data_M_annotation.rda")
 
+################################################################################
+# 2. visualization
+################################################################################
+data("data_M_annotation")
+library(dplyr)
 ov_palette =c('#A499CC',
               '#E0A7C8',
               '#E069A6',
@@ -33,8 +38,20 @@ ov_palette =c('#A499CC',
               "#F5D2A8",
               "#D2EBC8")
 
-p = CellRatioPlot(object = scRNA,
-                  sample.name = "Type",
-                  celltype.name = "celltype",
+p = CellRatioPlot(object = adata,
+                  sample.name = "disease_type",
+                  celltype.name = "subset_celltype",flow.curve = 0.3,
                   fill.col = ov_palette)
+p
 ggsave(p,filename=paste0("preanalysis",'/ratioplot.pdf'),width = 5,height = 6)
+
+adata <- RunUMAP(adata,reduction = 'scVI', dims = 1:30, verbose = FALSE)
+p1 = DimPlot(adata, group.by="subset_celltype", label=F, label.size=4.5, reduction='umap',cols = ov_palette )
+p1
+p2 = DimPlot(adata, group.by="subset_celltype", label=F, label.size=4.5, reduction='mde_M',cols = ov_palette )
+p2
+ggsave(p,filename=paste0("preanalysis",'/umap_anno.pdf'),width = 7,height = 6)
+
+
+
+
