0de8e3aca01e40ac47217770b8486ec1f3c70040
pigudogzyy <21301050051@m.fudan.edu.cn>
2024-05-21 21:41:03 +0800

    Update test.R

--- test.R
+++ test.R
@@ -28,6 +28,7 @@ save(adata,file="./data/data_M_annotation.rda")
 ################################################################################
 data("data_M_annotation")
 library(dplyr)
+library(Seurat)
 ov_palette =c('#A499CC',
               '#E0A7C8',
               '#E069A6',
@@ -38,14 +39,29 @@ ov_palette =c('#A499CC',
               "#F5D2A8",
               "#D2EBC8")
 
-p = CellRatioPlot(object = adata,
-                  sample.name = "disease_type",
-                  celltype.name = "subset_celltype",flow.curve = 0.3,
-                  fill.col = ov_palette)
-p
-ggsave(p,filename=paste0("preanalysis",'/ratioplot.pdf'),width = 5,height = 6)
+
+
+## Recluster and group
+set.seed(1314)
+# adata<- ScaleData(adata)
+# adata <- FindVariableFeatures(adata, selection.method = "vst", nfeatures = 2000)
+# adata<- RunPCA(adata)
+# plot1 <- DimPlot(adata, reduction = "pca", group.by="disease_type")
+# plot2 <- ElbowPlot(adata, ndims=30, reduction="pca")
+# plotc <- plot1+plot2
+# plotc
+#PC=30 as default
+pc.num=1:30
+
+adata <- FindNeighbors(adata, dims = pc.num,reduction = "scVI")
+adata <- FindClusters(adata,resolution = 0.3)
+table(adata@meta.data$seurat_clusters)
 
 adata <- RunUMAP(adata,reduction = 'scVI', dims = 1:30, verbose = FALSE)
+p1 = DimPlot(adata, group.by="seurat_clusters", label=F, label.size=4.5, reduction='umap',cols = ov_palette )
+p1
+
+
 p1 = DimPlot(adata, group.by="subset_celltype", label=F, label.size=4.5, reduction='umap',cols = ov_palette )
 p1
 p2 = DimPlot(adata, group.by="subset_celltype", label=F, label.size=4.5, reduction='mde_M',cols = ov_palette )
@@ -54,4 +70,11 @@ ggsave(p,filename=paste0("preanalysis",'/umap_anno.pdf'),width = 7,height = 6)
 
 
 
+
+p = CellRatioPlot(object = adata,
+                  sample.name = "disease_type",
+                  celltype.name = "subset_celltype",flow.curve = 0.3,
+                  fill.col = ov_palette)
+p
+ggsave(p,filename=paste0("preanalysis",'/ratioplot.pdf'),width = 5,height = 6)
 
